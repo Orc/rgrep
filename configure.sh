86371c62913a14e94c7475081de896323316d569
david parsons <orc@pie.pell.portland.or.us>
2017-04-17 06:50:29 -0700

    update configure.inc; add stfu code to configure.sh

--- configure.sh
+++ configure.sh
@@ -17,6 +17,22 @@ AC_INIT $TARGET
 AC_PROG_CC
 unset _MK_LIBRARIAN
 
+if [ "IS_BROKEN_CC" ]; then
+    case "$AC_CC $AC_CFLAGS" in
+    *-pedantic*) ;;
+    *)  # hack around deficiencies in gcc and clang
+	#
+	AC_DEFINE 'while(x)' 'while( (x) != 0 )'
+	AC_DEFINE 'if(x)' 'if( (x) != 0 )'
+
+	if [ "$IS_CLANG" ]; then
+	    AC_CC="$AC_CC -Wno-implicit-int"
+	elif [ "$IS_GCC" ]; then
+	    AC_CC="$AC_CC -Wno-return-type -Wno-implicit-int"
+	fi ;;
+    esac
+fi
+
 AC_CHECK_ALLOCA || AC_FAIL "$TARGET requires alloca()"
 AC_CHECK_FUNCS mmap
 AC_CHECK_FUNCS fgetln
